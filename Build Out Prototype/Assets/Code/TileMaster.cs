36522100a12fbb8a442bee2633f1a6af380f343f
MaxOrtGit <maxrort@gmail.com>
2021-10-28 07:50:16 -0500

    added clicking

--- Build Out Prototype/Assets/Code/TileMaster.cs
+++ Build Out Prototype/Assets/Code/TileMaster.cs
@@ -5,20 +5,7 @@ using UnityEngine;
 public class TileMaster : MonoBehaviour
 {
 
-    private void Update() { 
-        
-        if (Input.GetMouseButtonDown(0)) {  
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  
-            RaycastHit hit;  
-            if (Physics.Raycast(ray, out hit)) {  
-                if (hit.transform.tag == "Tile") {  
-                        print("tes2");
-                    //if key m is held down
-                    if (Input.GetKey(KeyCode.M)) {
-                        print("test");
-                    }
-                }   
-            }
-        }  
+    private void OnMouseDown() {
+        Destroy(gameObject);
     }
 }
